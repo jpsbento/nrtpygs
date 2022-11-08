2bd4e3d2c6c0ab4fa8ec5fc6082628e70212633c
Joao Bento <jpsbento@gmail.com>
2022-11-08 10:52:59 +0000

    a connection test script for testing connection to rabbitmq host

--- rmqclient/connection_test.py
+++ rmqclient/connection_test.py
@@ -1,23 +1,27 @@
 import os
 os.environ['SER_TLA'] = "TST"
 
-os.environ['RMQ_USER'] = "rmq"
-os.environ['RMQ_PASS'] = "rmq"
+os.environ['RMQ_USER'] = "rmq-admin"
+os.environ['RMQ_PASS'] = "rmq-admin"
 os.environ['RMQ_HOST'] = "localhost"
 
 from rmqclient.rmqproducer import RmqProducer
+from rmqclient.rmqtelemetry import RmqTelemetry
 
 producer = RmqProducer()
+#telemetry = RmqTelemetry()
 print("Finished setting up client")
+
 
 def submit_sequence():
     data = {
         "data": "test"
     }
     try:
-        #producer.call("TLA", "sequence", data)
         producer.produce(data)
-        producer.disconnect()
+        #telemetry.tel('Key','Value')
+        #producer.produce(data)
+        #producer.disconnect()
     except Exception as e:
         raise "Problem"
     return "Success"
